5c2bed789b5cf5718ee5cb9d8c11497dced2ca51
Aydin <aydinbg@gmail.com>
2013-09-17 14:36:30 +0200

    solved

--- Classic Algorithms/dynamic.rb
+++ Classic Algorithms/dynamic.rb
@@ -1,3 +1,5 @@
+# Solutions by Aydin Gungordu - www.aydingungordu.com
+
 require 'matrix'
 #A child is running up a staircase with n steps, and can hop either 1 step, 2 steps, 
 # or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
@@ -18,34 +20,16 @@ end
 # Does not work.
 
 class EightQueens
-	@@numQueens=0
-	@@chessboard=Matrix.build(8, 8) {|row, col| "." }
-	@@Queens=Array.new
 
-	def placeQueens(row)
-		gridsize = 8
-	
-		if (row== gridsize) 
-			 "FOUND RESULT!" 
-			 printBoard
-		else
-			gridsize.times do |col|
-				if(canInsert?(col,row))
-					@@chessboard.send(:[]=,col,row,"Q")
-					placeQueens(row + 1);
-				end
-			end
-		end
-	end
 
-	def printBoard
+	def printBoard(chessboard)
 		row=0
 		col=0
 		puts " -------------- "
 		while row<8 do 
 			col=0
 			while col < 8 do
-				print @@chessboard[row, col]
+				print chessboard[row, col]
 				col+=1
 			end
 			puts " "
@@ -54,19 +38,19 @@ class EightQueens
 		puts " -------------- "
 	end
 
-	def canInsert?(col,row)
-		puts rowSafe?(row)
-		puts colSafe?(col)
-		puts diagonalSafe?(col,row)
+	def canInsert?(col,row, chessboard)
+	#	puts rowSafe?(row, chessboard)
+	#	puts colSafe?(col, chessboard)
+	#	puts diagonalSafe?(col,row, chessboard)
 
-		return rowSafe?(row) && colSafe?(col) && diagonalSafe?(col,row)
+		return rowSafe?(row,chessboard) && colSafe?(col,chessboard) && diagonalSafe?(col,row,chessboard)
 	end
 
-	def rowSafe?(row)
+	def rowSafe?(row,chessboard)
 			col=0
 			while(col<8) do
-				if @@chessboard[col,row].to_s.eql?("Q")
-					puts "Row: %d unsafe on column %d" %[row,col]
+				if chessboard[col,row].to_s.eql?("Q")
+			#		puts "Row: %d unsafe on column %d" %[row,col]
 					return false
 				end
 			col+=1
@@ -74,25 +58,41 @@ class EightQueens
 		return true
 	end
 
-	def colSafe?(col)
+	def colSafe?(col,chessboard)
 		8.times do |row|
 			#print @@@chessboard[col,row]
-			if @@chessboard[col,row].to_s.eql?("Q")
-					puts "Col: %d unsafe" %col
+			if chessboard[col,row].to_s.eql?("Q")
+				#	puts "Col: %d unsafe" %col
 				return false
 			end
 		end
 		return true
 	end
 
-	def diagonalSafe?(col,row)
+	def placeQueens(row, chessboard)
+        0.upto(7) do |col|
+            if canInsert?(col, row, chessboard)
+               chessboard.send(:[]=,col,row,"Q")
+                if row == (7)
+                	puts "SOLUTION:"
+                    printBoard(chessboard)
+                else
+                    placeQueens(row+1, chessboard)
+                end
+                chessboard.send(:[]=,col,row,".")
+            end
+        end
+       # printBoard(chessboard)
+    end
+
+	def diagonalSafe?(col,row,chessboard)
 		checkCol=col
 		checkRow=row
 
 		while checkRow<8 && checkCol<8 do
 			checkRow+=1
 			checkCol+=1
-			if @@chessboard[checkCol,checkRow].eql?("Q")
+			if chessboard[checkCol,checkRow].eql?("Q")
 				return false
 			end
 		end
@@ -103,7 +103,7 @@ class EightQueens
 		while checkRow>=0 && checkCol<8 do
 			checkRow-=1
 			checkCol+=1
-			if @@chessboard[checkCol,checkRow].eql?("Q")
+			if chessboard[checkCol,checkRow].eql?("Q")
 				return false
 			end
 		end
@@ -114,7 +114,7 @@ class EightQueens
 		while checkRow<8 && checkCol>=0 do
 			checkRow+=1
 			checkCol-=1
-			if @@chessboard[checkCol,checkRow].eql?("Q")
+			if chessboard[checkCol,checkRow].eql?("Q")
 				return false
 			end
 		end
@@ -125,7 +125,7 @@ class EightQueens
 		while checkRow>=0 && checkCol>=0 do
 			checkRow-=1
 			checkCol-=1
-			if @@chessboard[checkCol,checkRow].eql?("Q")
+			if chessboard[checkCol,checkRow].eql?("Q")
 				return false
 			end
 		end
@@ -135,6 +135,8 @@ class EightQueens
 end
 
 eq=EightQueens.new
-eq.placeQueens(0)
+chessboard=Matrix.build(8, 8) {|row, col| "." }
+eq.placeQueens(0,chessboard)
+
 
 	
